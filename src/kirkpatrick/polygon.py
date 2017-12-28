396c0d920a53aaa3ef2977c1c48c1b50163cb658
forsooth <maastntes@gmail.com>
2017-12-28 18:05:12 -0500

    Remove repetitive files and move test.py out of package directory

--- src/kirkpatrick/polygon.py
+++
@@ -1,6 +0,0 @@
-class SimplePolygon:
-        def __init__(self, name, points, edges):
-                self.name = name
-                self.points = points  # arrange is a different way?
-                self.edges = edges  # arrange??
-
