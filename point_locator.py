d5c90fc599b3bda974e22cc12776d2ea70ecace7
forsooth <maastntes@gmail.com>
2017-12-28 17:56:23 -0500

    Add skeleton for point locator main functions

--- point_locator.py
+++ point_locator.py
@@ -1,18 +1,31 @@
+import functools
+
 class PointLocator:
 
     def __init__(self, polygons):
+        # Get as input a list of polygons
         self.polygons = polygons
+        # triangulate each polygon
+        trangs = [p.triangluate() for p in polygons]
+        # merge triangulations into one triangulation
+        T = functools.reduce(lamdba t1, t2: t1.merge(t2), None)
+        # TODO: possibly add 3 dummy points at extremes to fix convex hull
+        # treat triangulation as planar graph
+        P = PlanarGraph(T)
+        # initialize dag as a forest
+        self.D = DAG()
+        for t in T.triangles():
+            D.add_vertex(t, None)
+        while True:
+            S = P.find_indep_low_deg()
+            Tc = T.copy()
+            Pc = P.copy()
+            for v in S:
+                Tc.remove_point(v.value)
+                Pc.remove_vertex(v)
+            # TODO: retriangulate both efficiently
+            # TODO: look for overlaps and add to D
 
     def query(point):
-        """
-        Get as input a list of polygons
-        triangulate each polygon
-        merge triangulations into one triangulation
-        initialize dag as a forest
-        find independent set of low degree vertices
-        copy triangulation
-        remove ind set
-        retriangulate
-        look for overlaps and add to dag
-        """
-
+        cf = lambda x: x.value.contains(point)
+        return self.D.find_leaf_where(cf)
