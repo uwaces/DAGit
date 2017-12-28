2b6f7d45745a5581ee7fe4051e2b0f606aed34f2
uwaces <uwaces@gmail.com>
2017-12-28 17:22:55 -0500

    Continue testing...

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import csv
 from planar_graph import PlanarGraph
+import earcut
 
 polygon = []
 
@@ -19,3 +20,13 @@ with open('outer_triangle.csv', newline='') as csvfile:
 print("Outer Triangle: " + str(outer_triangle))
 
 pg = PlanarGraph()
+
+vertex_ids = [pg.addVertex(p[0], p[1]) for p in polygon]
+print(vertex_ids)
+
+vertex_ids_triangle = [pg.addVertex(p[0], p[1]) for p in outer_triangle]
+print(vertex_ids_triangle)
+
+input_to_earcut = earcut.flatten([polygon, outer_triangle])
+
+earcut.earcut(input_to_earcut["vertices"], input_to_earcut["holes"], input_to_earcut["dimensions"])
