a08a36dba34de42f199e6acee19fa4e717f58a42
uwaces <uwaces@gmail.com>
2017-12-29 04:50:48 -0500

    modifing test.py to include triangluation helper function

--- src/test.py
+++ src/test.py
@@ -1,6 +1,65 @@
 import csv
 from kirkpatrick import planar
 from kirkpatrick import earcut
+from kirkpatrick import dag as DAG
+def getTriangulation(graph, polygon, hole=None):
+        # Flatten the points
+        points = []
+        for p in polygon:
+                points.append(graph.vertices[p].point[0])
+                points.append(graph.vertices[p].point[1])
+
+        # Triangulate -- including hole
+        if hole is not None:
+                hole_begin = len(points) // 2
+                for p in hole:
+                        points.append(graph.vertices[p].point[0])
+                        points.append(graph.vertices[p].point[1])
+
+                triangulation = earcut.earcut(points, [hole_begin]);
+        else: 
+                triangulation = earcut.earcut(points)
+ 
+        # convert back into a list of triangles
+        triangles = []
+        for i in range(0, len(triangulation), 3): 
+                id1 = triangulation[i] 
+                id2 = triangulation[i+1]
+                id3 = triangulation[i+2]
+
+                p1 = polygon[id1] if id1 < len(polygon) else hole[id1 - len(polygon)] 
+                p2 = polygon[id2] if id2 < len(polygon) else hole[id2 - len(polygon)]
+                p3 = polygon[id3] if id3 < len(polygon) else hole[id3 - len(polygon)]
+
+                triangles.append([p1, p2, p3])
+
+        return triangles 
+
+# Triangulate a polygon and fix graph appropriately
+def triangulate(graph, polygon, hole=None):
+        # magic triangulation
+        triangles = getTriangulation(graph, polygon, hole)
+
+        # connect the graph / add triangles to the points / return the triangle id's 
+        new_triangle_ids = []
+        for t in triangles:
+                graph.connect(t[0], t[1])
+                graph.connect(t[1], t[2]) 
+                graph.connect(t[2], t[0]) 
+
+                # create new triangle 
+                tri = planar.Triangle(t[0], t[1], t[2])
+                triangle_id = len(graph.all_triangles)
+                new_triangle_ids.append(triangle_id)  
+                graph.all_triangles.append(tri) 
+
+                # add new triangle to each of the three vertices' lists 
+                graph.vertices[t[0]].addTriangle(triangle_id) 
+                graph.vertices[t[1]].addTriangle(triangle_id) 
+                graph.vertices[t[2]].addTriangle(triangle_id) 
+
+        return new_triangle_ids 
+
 
 polygon = []
 
@@ -22,28 +81,34 @@ print("Outer Triangle: " + str(outer_triangle))
 
 pg = planar.PlanarGraph()
 
-vertex_ids = [pg.addVertex(p[0], p[1]) for p in polygon]
-print("Vertex IDs of polygon: " + str(vertex_ids))
 
+# Order matters so that vertex id's match
 vertex_ids_triangle = [pg.addVertex(p[0], p[1]) for p in outer_triangle]
 print("Vertex IDs of outer triangle: " + str(vertex_ids_triangle))
 
+vertex_ids = [pg.addVertex(p[0], p[1]) for p in polygon]
+print("Vertex IDs of polygon: " + str(vertex_ids))
 
-print("Find the triangles that are the outside the polygon:")
-input_to_earcut = earcut.flatten([outer_triangle, polygon])
-print("Ear-cut input: " + str(input_to_earcut))
+# Connect up the polygon
+for v_i in range(1, len(vertex_ids)):
+        pg.connect(vertex_ids[v_i], vertex_ids[v_i-1])
+pg.connect(vertex_ids[-1], vertex_ids[0])
 
+# Get the inner triangles:
+triangles = triangulate(pg, vertex_ids)
 
+print("Inner triangle id's: " + str(triangles))
 
-output_earcut = earcut.earcut(input_to_earcut["vertices"], input_to_earcut["holes"], input_to_earcut["dimensions"])
-print("Earcut Triangles output (3 ids, 3ids....): " + str(output_earcut))
+# Connect up the outer triangle
+for v_i in range(1, len(vertex_ids_triangle)):
+        pg.connect(vertex_ids_triangle[v_i], vertex_ids_triangle[v_i-1])
+pg.connect(vertex_ids_triangle[-1], vertex_ids_triangle[0])
 
+outer_triangles = triangulate(pg, vertex_ids_triangle, vertex_ids)
 
-print("Find the triangles from inside the polygon")
-p_list = earcut.flatten([polygon, []])
-p_triangles = earcut.earcut(p_list["vertices"], dim=p_list["dimensions"])
-print(p_list)
-print(p_triangles)
+print("Outer Triangle ID's " + str(outer_triangles))
+
+dag = DAG.DAG()
 
 
 
